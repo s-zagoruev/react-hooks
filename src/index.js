365882809498152e68efc17c718b3cad4b35de2f
s-zagoruev <zagoruev@gmail.com>
2021-06-29 13:42:44 +0300

    Install react-app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <p>Hi</p>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
