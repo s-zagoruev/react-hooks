90449eb194d3d1ab3ca8ef5b3cff628c632d439f
s-zagoruev <zagoruev@gmail.com>
2021-07-09 17:17:21 +0300

    Hook useCallback

---
+++ src/use-callback.js
@@ -0,0 +1,54 @@
+import {useEffect, useState, useCallback} from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+  const [value, setValue] = useState(1)
+  const [visible, setVisible] = useState(true)
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue(v => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <PlanetInfo id={value}/>
+      </div>
+    )
+  } else {
+    return <button onClick={() => setVisible(true)}>show</button>
+  }
+}
+
+const getPlanet = (id) => {
+  return fetch(`https://swapi.dev/api/planets/${id}`)
+    .then(res => res.json())
+    .then(data => data)
+}
+
+const useRequest = (request) => {
+  const [dataState, setDataState] = useState(null)
+
+  useEffect(() => {
+    let cancelled = false
+    request().then(data => !cancelled && setDataState(data))
+
+    return () => cancelled = true
+  }, [request])
+
+  return dataState
+}
+
+const usePlanetInfo = (id) => {
+  const request = useCallback(() => getPlanet(id), [id])
+  return useRequest(request)
+}
+
+const PlanetInfo = ({id}) => {
+  const data = usePlanetInfo(id)
+
+  return (
+    <div>{id} - {data && data.name}</div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
